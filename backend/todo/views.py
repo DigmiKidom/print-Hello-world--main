692ed9893b972a00f33dec7011b3f9e48228bab2
{user_name} <omridig48@gmail.com>
2026-06-07 19:59:40 +0300

    first-commit

---
+++ backend/todo/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+   
+    if request.method == 'POST':
+        task_title = request.POST.get('title')
+        if task_title:
+            Task.objects.create(title=task_title)
+        return redirect('task_list')
+
+    
+    tasks = Task.objects.all().order_by('-created_at')
+
+   
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
